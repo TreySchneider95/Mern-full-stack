d1eca7b2594a6235962d173d6f69085d797f0949
TreySchneider95 <tschneid95@gmail.com>
2023-03-13 08:44:25 -0700

    added app.js

---
+++ full-stack-server/app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+
+const app = express()
+const port = process.env.PORT || 8000
+
+app.get('/', (req, res) => res.send("Hello World!"));
+
+app.listen(port, ()=> console.log(`server running on port ${port}`))
